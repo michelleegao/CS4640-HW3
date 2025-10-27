8c24448db4c2c33a52a8daa2a1c906ec0c02b591
hpanjshiri <145395899+hpanjshiri@users.noreply.github.com>
2025-10-27 11:03:54 -0400

    fixed welcome missing bracket

--- views/welcome.php
+++ views/welcome.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Anagrams Welcome/title>
+    <title>Anagrams Welcome</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         body{
